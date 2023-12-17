c58d368b93571a1ae1a5da3617e2126409b3c068
mila2999 <mila.savonik@gmail.com>
2023-12-17 11:58:05 +0200

    Mnaging State, added Todo array dinamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 import NewTodo from './components/NewTodo';
 
 function App() {
-  const todos = [new Todo('Learn React '), new Todo('Learn TypeScript')];
-  const addTodoHandler = (todoText: string) => {};
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((curTodo) => {
+      return curTodo.concat(newTodo);
+    });
+  };
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
